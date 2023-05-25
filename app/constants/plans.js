10583a25e100d4de728edbaa915c414eb18f5cac
Xuxão <lucas.stoliveira@stone.com.br>
2023-05-25 17:15:28 -0400

    adptando para receber novos planos

---
+++ app/constants/plans.js
@@ -0,0 +1,4 @@
+module.exports = {
+    standard: { cashInMinibar: false, cashInBreakfast: true, totalDiscount: 0.3 },
+    free: { cashInMinibar: true, cashInBreakfast: true, totalDiscount: 0 }
+}
--- app/hotel.js
+++ app/hotel.js
@@ -1,35 +1,60 @@
 const extraFees = require('./constants/extraFees')
+const plans = require('./constants/plans')
 const rooms = require('./rooms.json')
 
 
 function calculateInvoice({ nights, roomName, state, isSubscriber, minibarConsumed, breakfastAdded }) {
+
   const room = rooms[roomName]
+  const plan = isSubscriber ? plans.standard : plans.free
 
-  const roomPrice = room.price * nights
+  return {
+    roomPrice: calculateRoomPrice({ nights, room }),
+    minibar: calculateMinibarFee({ room, minibarConsumed }, plan),
+    breakfast: calculateBreakfastFee({ breakfastAdded }, plan),
+    total: calculateTotal({ nights, minibarConsumed, breakfastAdded, room }, plan, state),
+  }
+}
 
-  var minibarFee = minibarConsumed && room.minibarFee ? room.minibarFee : 0
-  if (isSubscriber) {
+function calculateFees({ minibarConsumed, breakfastAdded, room }, plan) {
+
+  return calculateBreakfastFee({ breakfastAdded }, plan) + calculateMinibarFee({ room, minibarConsumed }, plan)
+}
+
+function calculateMinibarFee({ room, minibarConsumed }, plan) {
+  let minibarFee = minibarConsumed && room.minibarFee ? room.minibarFee : 0
+
+  if (!plan.cashInMinibar) {
     minibarFee = 0
   }
 
-  const breakfastFee = breakfastAdded ? 2500 : 0
+  return minibarFee
+}
 
-  // Calculate the total amount of the invoice
-  var total = roomPrice + minibarFee + breakfastFee
-  if (isSubscriber) {
-    total = total - (total * 0.3)
+function calculateBreakfastFee({ breakfastAdded }, plan) {
+  let breakfastPrice = 2500
+
+  if (!plan.cashInBreakfast || !breakfastAdded) {
+    breakfastPrice = 0
   }
+  return breakfastPrice
+}
 
-  const fees = extraFees[state.toUpperCase()] || extraFees.default
+function calculateTotal({ minibarConsumed, breakfastAdded, room, nights }, plan, state) {
+  let total = calculateRoomPrice({ room, nights }) + calculateFees({ minibarConsumed, breakfastAdded, room }, plan)
 
-  total = total + (total * fees)
 
-  return {
-    roomPrice: roomPrice,
-    minibar: minibarFee,
-    breakfast: breakfastFee,
-    total: total,
-  }
+  total = total - (total * plan.totalDiscount)
+
+
+  const localizationFees = extraFees[state.toUpperCase()] || extraFees.default
+
+  total = total + (total * localizationFees)
+  return total
+}
+
+function calculateRoomPrice({ room, nights }) {
+  return room.price * nights
 }
 
 module.exports = calculateInvoice
