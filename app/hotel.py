4d80544339b1c10d968b27da82ef7b955382d52b
Thiago Lima <e10.thiago@gmail.com>
2023-05-25 17:31:02 -0300

    refactor: reorganize params in taxation function

--- app/hotel.py
+++ app/hotel.py
@@ -2,18 +2,15 @@ from app.models.stay_informations import StayInformations
 
 
 def calculateInvoice(stay_information: StayInformations):
-    # Calculate the total amount of the invoice
-    total = apply_taxation(stay_information, stay_information.calculate_total_amount())
-
     return {
         'roomPrice': stay_information.calculate_room_price(),
         'minibar': stay_information.calculate_minibar_fee(),
         'breakfast': stay_information.calculate_breakfast_fee(),
-        'total': total,
+        'total': apply_taxation(stay_information.calculate_total_amount(), stay_information.state),
     }
 
 
-def apply_taxation(stay_information, total):
-    if stay_information.state == 'SP':
-        return total + total * 0.02
-    return total + total * 0.03
+def apply_taxation(amount, state):
+    if state == 'SP':
+        return amount + amount * 0.02
+    return amount + amount * 0.03
--- app/rooms.py
+++ app/rooms.py
@@ -1,21 +1,5 @@
 from dataclasses import dataclass
 
-ROOMS = {
-    "standard": {
-        "price": 9000,
-        "hasHotTub": False
-    },
-    "deluxe": {
-        "price": 14000,
-        "hasHotTub": False,
-        "minibarFee": 7500
-    },
-    "presidential": {
-        "price": 17000,
-        "hasHotTub": True,
-        "minibarFee": 9500
-    }
-}
 
 @dataclass
 class Room:
