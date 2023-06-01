576d32d04d3f27ccc8068aab0910e8ddfb7dbb84
Xuxão <lucas.stoliveira@stone.com.br>
2023-06-01 15:27:16 -0400

    add plan platinum

--- app/constants/plans.js
+++ app/constants/plans.js
@@ -1,4 +1,5 @@
 module.exports = {
+    platinum: { cashInMinibar: false, cashInBreakfast: true, totalDiscount: 0.45 },
     gold: { cashInMinibar: false, cashInBreakfast: true, totalDiscount: 0.3 },
     free: { cashInMinibar: true, cashInBreakfast: true, totalDiscount: 0 }
 }
