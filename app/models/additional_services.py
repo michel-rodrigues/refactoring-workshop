b99aa3b50600296ed97c2cc34d4bd99260a887e4
Thiago Lima <e10.thiago@gmail.com>
2023-06-01 17:06:30 -0300

    create breakfast additional service

--- app/models/additional_services.py
+++ app/models/additional_services.py
@@ -1,12 +1,21 @@
 from dataclasses import dataclass
+from app.models.subs import Subscription
 
 
 @dataclass
 class AdditionalService:
     name: str
-    price: int
+    base_price: int
+
+    def get_fee(self, subscription: Subscription):
+        return self.base_price
 
 
-MASSAGE = AdditionalService(name="massage", price=12000)
-SAUNA = AdditionalService(name="sauna", price=3000)
-BREAKFAST = AdditionalService(name="breakfast", price=2500)
+class Breakfast(AdditionalService):
+    def get_fee(self, subscription: Subscription):
+        return self.base_price if not subscription.breakfastComplimentary else 0
+
+
+MASSAGE = AdditionalService(name="massage", base_price=12000)
+SAUNA = AdditionalService(name="sauna", base_price=3000)
+BREAKFAST = Breakfast(name="breakfast", base_price=2500)
